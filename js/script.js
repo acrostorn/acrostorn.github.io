15e5d54be9f9164545297a331c14ba87f1021ad8
acrostorn <rijuiswtr@gmail.com>
2025-03-29 22:44:27 +0900

    3/29 1st

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// js/script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const faders = document.querySelectorAll('.fade-in');
+  
+    const appearOptions = {
+      threshold: 0.2,
+      rootMargin: "0px 0px -50px 0px"
+    };
+  
+    const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, appearOptions);
+  
+    faders.forEach(fader => {
+      appearOnScroll.observe(fader);
+    });
+  });
+  
